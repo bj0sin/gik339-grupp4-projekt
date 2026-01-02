b1f728adac405610e66942ae9a057d1330604f04
toveef <tove.fahlcrantz@gmail.com>
2026-01-02 15:53:45 +0100

    allt rätt klart

--- script.js
+++ script.js
@@ -16,17 +16,21 @@
              movies.forEach(movies =>{
               html += ` 
               <li
-                 class="bg-green-200 basis-1/4 text-green-900 p-2 rounded-md
-                 border-2 border-green-400 flex flex-col justify-between">
-                 <h3> Title: ${movies.movieTitle} Längd: ${movies.runTime} min Utgiven: ${movies.movieYear}</h3>
+                 class="bg-black/25 basis-1/4 text-white-900 p-2 rounded-md
+                  flex flex-col justify-between">
+                 <h3>
+  <span class="block">Titel: ${movies.movieTitle}</span>
+  <span class="block">Längd: ${movies.runTime} min</span>
+  <span class="block">Utgiven: ${movies.movieYear}</span>
+</h3>
                  <p>Genre: ${movies.genre}</p>
                  <div>
                      <button
-                         class="border border-green-300 hover:bg-white/100
+                         class=" hover:bg-white/25
                          rounded-md/50 bg-white/50 p-1 text-sm mt-2" onclick="setCurrentMovie(${movies.id})">
                          Ändra
                      </button>
-                     <button class="border border-green-300 hover:bg-white/100
+                     <button class=" hover:bg-white/25
                          rounded-md/50 bg-white/50 p-1 text-sm mt-2" onclick="deleteMovie(${movies.id})">
                          Ta bort
                      </button>
